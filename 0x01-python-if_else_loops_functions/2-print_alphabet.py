ef04bc0da6efed382d6dd35b71484a7c3bf5cbbc
Vik-Akachi <victorakachi89@gmail.com>
2023-03-07 06:27:06 -0800

    0

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#to print the letters of the alphabet in lower case from theie ASCII values
+# to print the letters of the alphabet in lower case from theie ASCII values
 for i in range(97, 123):
     print('{}' .format(chr(i)), end="")
-
