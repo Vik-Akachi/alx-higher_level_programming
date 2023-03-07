a4518cd1c5aa222c7f3e56a78fc44cd524714d26
Vik-Akachi <victorakachi89@gmail.com>
2023-03-07 15:31:21 -0800

    0

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 100):
-        if (i % 3 == 0):
+    for i in range(1, 101):
+        if ((i % 5 == 0) and (i % 3 == 0)):
+            print('FizzBuzz', end=" ")
+        elif (i % 3 == 0):
             print('Fizz', end=" ")
         elif (i % 5 == 0):
             print('Buzz', end=" ")
@@ -9,5 +11,5 @@ def fizzbuzz():
             print('FizzBuzz', end=" ")
         else:
             print(f'{i}', end=" ")
-    print('\n', end="")
+    print('', end="$")
 
