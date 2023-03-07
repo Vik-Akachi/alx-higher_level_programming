a7150d684db0e21e4112363222aae32e6a362cbf
Vik-Akachi <victorakachi89@gmail.com>
2023-03-07 12:31:59 -0800

    0

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-def islower(c):
+#def islower(c):
 
-    if ord(c) >= 97 and ord(c) <= 122:
-        return True
+#   if ord(c) >= 97 and ord(c) <= 122:
+#        return True
+#   else:
+#        return False
+def islower(c):
+    character = ord(i)
+    if ((i > 64) or (i < 91)):
+        print('{} is lower' .format(ord(i)))
+        return(True)
     else:
-        return False
+        print('{} is upper' .format(ord(i)))
+        return(False)
