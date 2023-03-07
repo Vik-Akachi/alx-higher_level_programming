c7dec3a9e764501f9b8471474da0fbf3f90d4af6
Vik-Akachi <victorakachi89@gmail.com>
2023-03-07 06:43:12 -0800

    0

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # the letters of the lower case alphabets using ASCII value except e and q
 for i in range(97, 123):
-    # using the or operator to tell the program to skip ASCII no.s 101 or 113 
+    # using the or operator to tell the program to skip ASCII no.s 101 or 113
     if ((i == 101) or (i == 113)):
         i += 1
         continue
-    print('{}' .format(chr(i)), end="") 
+    print('{}' .format(chr(i)), end="")
