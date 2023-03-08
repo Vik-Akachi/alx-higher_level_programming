3d32c3e84c2ae83b6056bb453312be507b37fdee
Vik-Akachi <victorakachi89@gmail.com>
2023-03-07 16:12:18 -0800

    0

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def rev(chr1):
+   # char1 = chr2
+    for i in chr1:
+        if ord(i) >= 65 and ord(i) <= 122:
+            i -= 1
+            if ord(j) == ord(i + 32):
+                j -= 2
+            print(f'{i}, {j}', end="")
+# rev(chr1)
