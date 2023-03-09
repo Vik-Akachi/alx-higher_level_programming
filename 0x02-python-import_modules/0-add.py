fd3a43422b8c299417626812aec5cf98addaa192
Vik-Akachi <victorakachi89@gmail.com>
2023-03-09 13:13:25 -0800

    0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
+# set an iteration loof with the if function
+# link function to the main function
 if __name__ == "__main__":
+    # import the add module form main module
     from  add_0 import add
 
     a = 1
     b = 2
+
     print('{} + {} = {}' .format(a, b, add(a, b)))
