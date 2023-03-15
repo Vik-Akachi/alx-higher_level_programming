fb256b43a9dda161776dd9c31ac6a2d65ae22ae2
Vik-Akachi <victorakachi89@gmail.com>
2023-03-14 23:21:36 -0700

    0

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    i = 0
+    while i >= 1:
+        if idx < 0:
+            return (none)
+        elif idx > i:
+            return (none)
+        else:
+            return (i)
