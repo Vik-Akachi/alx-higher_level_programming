70460a54f76942f3c2f30923d653b3de2a579d5f
Vik-Akachi <victorakachi89@gmail.com>
2023-03-14 23:36:28 -0700

    0

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    i = 0
-    while i >= 1:
+    while idx < len(my_list):
         if idx < 0:
             return (none)
-        elif idx > i:
+        elif idx > len(my_list):
             return (none)
         else:
-            return (i)
+            return (idx + 1)
