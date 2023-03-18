a0cda05ed56a2c62021381dfab7eca16b8d768bc
Vik-Akachi <victorakachi89@gmail.com>
2023-03-18 16:13:49 -0700

    0

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,16 +4,18 @@ if __name__ == "__main__":
     # import the system function to handle argumentrs
     import sys
 
-    # introduce iteration argument to count the number os strings/arguments given
+    # introduce iteration argument to count the number os
+    # strings/arguments given
     nums = len(sys.argv)
 
-    # loop throuh the argumrnts 
+    # loop throuh the argumrnts
     if (nums == 1):
         print("{} arguments." .format(nums - 1))
     elif (nums == 2):
         print("{} argument:" .format(nums - 1))
     else:
         print("{} arguments:" .format(nums - 1))
-        i = 1
-        while i < nums:
-            print("{}: {}" .format((i), nums[i]))
+    i = 1
+    while i < nums:
+        print("{}: {}" .format((i), sys.argv[i]))
+        i += 1
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-def sum():
-    for i in range():
-        while range[i]:
-            i = i + i
-        return (range[i] +i)
+
