5216f0ee8a6e34e2580a27f84eef5b96be4a74f2
Vik-Akachi <victorakachi89@gmail.com>
2023-03-18 15:52:31 -0700

    0

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,14 +5,15 @@ if __name__ == "__main__":
     import sys
 
     # introduce iteration argument to count the number os strings/arguments given
-    number = len(sys.argv) -1
+    nums = len(sys.argv)
 
     # loop throuh the argumrnts 
-    if (argv == 0):
-        print("o arguments")
-    elif (argv == 1):
-        print("1 argument")
-    elif (argv > 1):
-        print("{} arguments" .format(number))
-    for i in range(number):
-            print("{}: {}" .format(i + 1, sys.argv[i + 1]))
+    if (nums == 1):
+        print("{} arguments." .format(nums - 1))
+    elif (nums == 2):
+        print("{} argument:" .format(nums - 1))
+    else:
+        print("{} arguments:" .format(nums - 1))
+        i = 1
+        while i < nums:
+            print("{}: {}" .format((i), nums[i]))
