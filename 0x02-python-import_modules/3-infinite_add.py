f6d6941c059cc8250fcf5365622852e1eaf8f671
Vik-Akachi <victorakachi89@gmail.com>
2023-03-18 16:41:58 -0700

    1

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+import sys
 
+if __name__ == "__main__":
+    sys.argv.pop(0)
+    nums = sys.argv
+    sum_total = 0
+    for i in nums:
+        sum_total += int(i)
+    print("{}" .format(sum_total))
