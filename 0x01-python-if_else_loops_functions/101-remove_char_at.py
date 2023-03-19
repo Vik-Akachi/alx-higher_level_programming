6b3ebb8dbc93fb4d667dc881ff79b4ab32863f1b
Vik-Akachi <victorakachi89@gmail.com>
2023-03-19 14:49:07 -0700

    1

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+   # new_str = " "
+    for i in range(len(str)):
+        if str[i] != str[n]:
+           # continue
+            i += 1
+        return(str)
+
+       # new_str = str[i] - str[n]
+       # print('{}' .format(str[i]), end="")
---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import calculatore_1
+
+    argv == 3
+
+    add = calulator_1.add(a, b)
+    if argv != 3:
+        print("{} + {}".format(int(a), int(b)))
+    else:
+        print("{} + {} = {}".format((int(a)), (int(b)), (int(c))))
+    
+    sub = calculator_1.sub(a, b)
+    if argv != 3:
+        print("{} - {}".format(int(a), int(b)))
+    else:
+        print("{} - {} = {}".format((int(a)), (int(b)), (int(c))))
+
+    mult = calculator_1.mul(a, b)
+    if argv != 3:
+        print("{} * {}".format(int(a), int(b)))
+    else:
+        print("{} * {} = {}".format((int(a)), (int(b)), (int(c))))
+
+    div = calculator_1(a, b)
+    if argv != 3:
+        print("{} / {}".format(int(a), int(b)))
+    else:
+        print("{} / {} = {}".format((int(a)), (int(b)), (int(c))))
+
