c9bdd44cf6c94730dbe79b5f10eef18204532924
Vik-Akachi <victorakachi89@gmail.com>
2023-03-27 13:56:45 -0700

    2

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,13 +2,10 @@
 
 def safe_print_integer(value):
 
-#    i = 0
-#    for i in value:
-
     try:
         print("{:d}".format((value)))
         while int(value):
-            return True
+            return (True)
 
     except ValueError:
-        return False
+        return (False)
