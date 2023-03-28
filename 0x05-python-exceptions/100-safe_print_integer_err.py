7f8e2d61516a47d70a0ae021ac7713bccb9d4b64
Vik-Akachi <victorakachi89@gmail.com>
2023-03-28 05:10:14 -0700

    3

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+
+def safe_print_integer_err(value):
+    """prints integers with "{:d}" .format().
+
+    if the value to be printed is an integer, print it
+
+    if not: if the ValueError message engaged a given message is printed
+    to the standard error (stderr).
+    """
+
+    try:
+                print("{:d}".format(value))
+
+                return (True)
+
+    except (ValueError, TypeError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
