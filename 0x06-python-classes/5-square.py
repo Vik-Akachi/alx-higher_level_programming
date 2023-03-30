75b0f6a931528410e62fd3071209a3c1d91866ee
Vik-Akachi <victorakachi89@gmail.com>
2023-03-30 14:09:16 -0700

    %

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -30,9 +30,8 @@ class Square:
         return(self.__size * self.__size)
 
     def my_print(self):
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
         if self.__size == 0:
             print(" ")
-        else:
-            for i in range(0, self.__size):
-                [print("#", end="") for j in range(self.__size)]
-                print("")
