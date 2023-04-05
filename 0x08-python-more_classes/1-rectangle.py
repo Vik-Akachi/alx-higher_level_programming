7c181a10582996a798d475cc939ed582e3b05462
Vik-Akachi <victorakachi89@gmail.com>
2023-04-04 17:28:29 -0700

    2

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,12 +4,18 @@
 
 class Rectangle:
     """
-    Initialising the given Rectangle with attributes
+    Rectangle initialised with the following attributes
     Private 'width' and
     private height
     Args:
-    self.__width = width
-    self.__height = height
+        width (int) : width
+        height (int) : height
+    Functions:
+        __init__(self, width, height)
+        width(self)
+        width(self, value)
+        height(self)
+        height(self, value)
     """
 
     def __init__(self, width=0, height=0):
