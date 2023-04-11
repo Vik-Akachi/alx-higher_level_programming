470d8a2f9a18dbfa710a18482f86b9cceab75f6b
Vik-Akachi <victorakachi89@gmail.com>
2023-04-11 07:52:12 -0700

    4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -80,10 +80,10 @@ class Rectangle:
                 rectangle.append("\n")
         return ("".join(rectangle))
 
-        def __repr__(self):
-            """
-            This Returns the string representation of the Rectangle
-            """
-            rectangle = "Rectangle(" + str(self.__width)
-            rectangle += ", " + str(self.__height) + ")"
-            return (rectangle)
+    def __repr__(self):
+        """
+        This Returns the string representation of the Rectangle
+        """
+        rectangle = "Rectangle(" + str(self.__width)
+        rectangle += ", " + str(self.__height) + ")"
+        return (rectangle)
