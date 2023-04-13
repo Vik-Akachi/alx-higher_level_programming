c62d0c7175a0859f2a6ec23e013337501fb283fa
Vik-Akachi <victorakachi89@gmail.com>
2023-04-13 13:57:09 -0700

    1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,4 +5,4 @@
 def write_file(filename="", text=""):
     """this code returns the byte count of the strings"""
     with open(filename, 'w', encoding="utf=8") as f:
-        return f.write(text) 
+        return f.write(text)
